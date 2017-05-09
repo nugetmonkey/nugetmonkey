9a263ef1b8a772e5ca155864d444fe9f1ca7bcef
mehmet <mehmet.yucebitekanalytics.com>
2017-05-09 10:48:49 +0300

    first commit

---
+++ Test1/Program.cs
@@ -0,0 +1,29 @@
+﻿using org.apache.camel;
+using org.apache.camel.builder;
+using org.apache.camel.impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    public class Rb:RouteBuilder
+    {
+        public override void configure()
+        {
+            from("file://test1").to("file://test2");
+        } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CamelContext context = new DefaultCamelContext();
+            context.addRoutes(new Rb() );
+            context.start();
+            System.Threading.Thread.CurrentThread.Join();
+        }
+    }
+}
